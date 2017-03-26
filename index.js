786e1cb7d8379b65c10c7c025f08150ab10ef1df
Jim Bulkowski <jim.b@paperelectron.com>
2017-03-26 11:34:59 -0400

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project pom-test-stubs
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+"use strict";
+
+exports.mockFrameworkInjector = require('./lib/mockFrameworkInjector')
+exports.buildPkgJson = require('./lib/buildPkgJson')
+exports.findMockDir = require('./lib/findMockDir')
+exports.registerMocks = require('./lib/registerMocks')
+exports.mockPlugin = require('./lib/mockPlugin')
